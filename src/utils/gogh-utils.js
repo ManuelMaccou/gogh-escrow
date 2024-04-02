a27813a77f338034e6727e730cd8eace79daa466
Mona Lisa <jennifer@Mac-mini.local>
2024-04-02 20:11:14 +0200

    Update; gogh front-end utils preparation - wip.

---
+++ src/utils/gogh-utils.js
@@ -0,0 +1,22 @@
+const hasEscrowExpired = (escrowId) => {
+  return new Promise((resolved, rejected) => {});
+};
+
+const signEscrowSignature = (escrowData) => {
+  return new Promise((resolved, rejected) => {});
+};
+
+const getEscrowDetails = (escrowId) => {
+  return new Promise((resolved, rejected) => {});
+};
+
+const getEscrowLogs = (escrowId) => {
+  return new Promise((resolved, rejected) => {});
+};
+
+module.exports = {
+  hasEscrowExpired,
+  signEscrowSignature,
+  getEscrowDetails,
+  getEscrowLogs,
+};
