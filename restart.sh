15bf53df5d22eff984cdc45eef6953da25a74086
Mona Lisa <jennifer@jennifers-MacBook-Air.local>
2024-04-03 17:22:59 +0200

    Update

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo Reloading Gogh Daemon...;
-cp -rf /root/server/gogh-oracle.service /etc/systemd/system
-cp -rf /root/server/gogh-server.service /etc/systemd/system
+cp -rf /root/gogh-backend/gogh-oracle.service /etc/systemd/system
+cp -rf /root/gogh-backend/gogh-server.service /etc/systemd/system
 systemctl daemon-reload
 systemctl restart gogh-oracle.service
 systemctl restart gogh-server.service
