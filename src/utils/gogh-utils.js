e1f9ec88535c9a5e81e6d63e7de7b24dd2803a4d
Mona Lisa <jennifer@jennifers-MacBook-Air.local>
2024-04-03 12:32:06 +0200

    Update

--- src/utils/gogh-utils.js
+++ src/utils/gogh-utils.js
@@ -1,18 +1,74 @@
-const signEscrowSignature = (escrowData) => {
-  return new Promise((resolved, rejected) => {});
-};
+const ethers = require("ethers");
 
-const getEscrowDetails = (escrowId) => {
-  return new Promise((resolved, rejected) => {});
-};
+module.exports = class GoghUtilities {
+  apiUrl = "http://localhost";
 
-const getEscrowLogs = (escrowId) => {
-  return new Promise((resolved, rejected) => {});
-};
+  constructor(goghServerApiUrl) {
+    this.apiUrl = goghServerApiUrl;
+  }
 
-module.exports = {
-  hasEscrowExpired,
-  signEscrowSignature,
-  getEscrowDetails,
-  getEscrowLogs,
+  signEscrowSignature(escrowData) {
+    return new Promise((resolved, rejected) => {
+      const payload = ethers.utils.defaultAbiCoder.encode(
+        ["address", "address", "uint256", "address", "address"],
+        [
+          escrowData.escrowId,
+          escrowData.token,
+          escrowData.amount,
+          escrowData.recipient,
+          escrowData.owner,
+        ]
+      );
+      const escrowMessageHash = ethers.utils.keccak256(payload);
+      const messageHashBinary = ethers.utils.arrayify(escrowMessageHash);
+      sellerWallet
+        .signMessage(messageHashBinary)
+        .then((r) => {
+          resolved(r);
+        })
+        .catch((e) => {
+          rejected(e);
+        });
+    });
+  }
+
+  getEscrowDetails(escrowId) {
+    return new Promise((resolved, rejected) => {
+      fetch(`${this.apiUrl}/get_escrow_details/${escrowId}`, {
+        method: "GET",
+      })
+        .then(async (r) => {
+          if (r.status !== 200) {
+            const error = await r.text();
+            throw error;
+          }
+          const response = await r.json();
+          resolved(response);
+          return true;
+        })
+        .catch((e) => {
+          rejected(e);
+        });
+    });
+  }
+
+  getEscrowLogs(escrowId) {
+    return new Promise((resolved, rejected) => {
+      fetch(`${this.apiUrl}/get_escrow_logs/${escrowId}`, {
+        method: "GET",
+      })
+        .then(async (r) => {
+          if (r.status !== 200) {
+            const error = await r.text();
+            throw error;
+          }
+          const response = await r.json();
+          resolved(response);
+          return true;
+        })
+        .catch((e) => {
+          rejected(e);
+        });
+    });
+  }
 };
